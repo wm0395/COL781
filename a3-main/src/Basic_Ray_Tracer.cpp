d2e9688d64dd02b0dd1d880d50c33b1a1f5f86a1
wm0395 <wmharshit@gmail.com>
2024-03-12 18:21:15 +0530

    init

--- a3-main/src/Basic_Ray_Tracer.cpp
+++ a3-main/src/Basic_Ray_Tracer.cpp
@@ -8,22 +8,22 @@ bool Ray_Tracer::initialize(const std::string &title, int width, int height, int
             printf("SDL could not initialize! SDL_Error: %s", SDL_GetError());
             success = false;
         }
-        // } else {
-        //     this->frameHeight = height;
-        //     this->frameWidth = width;
-        //     this->displayScale = 1;
-        //     this->spp = spp;
-        //     int screenWidth = frameWidth * displayScale;
-        //     int screenHeight = frameHeight * displayScale;
-        //     window = SDL_CreateWindow(title.c_str(), SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, width, height, SDL_WINDOW_SHOWN);
-        //     if (window == NULL) {
-        //         printf("Window could not be created! SDL_Error: %s", SDL_GetError());
-        //         success = false;
-        //     } else {
-        //         this->windowSurface = SDL_GetWindowSurface(window);
-        //         this->framebuffer = SDL_CreateRGBSurface(0, this->frameWidth, this->frameHeight, 32, 0, 0, 0, 0);
-        //         this->pointBuffer = std::vector<std::vector<Attribs>>(this->frameWidth, std::vector<Attribs>(this->frameHeight, Attribs()));
-        //     }
-        // }
+        } else {
+            this->frameHeight = height;
+            this->frameWidth = width;
+            this->displayScale = 1;
+            this->spp = spp;
+            int screenWidth = frameWidth * displayScale;
+            int screenHeight = frameHeight * displayScale;
+            window = SDL_CreateWindow(title.c_str(), SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, width, height, SDL_WINDOW_SHOWN);
+            if (window == NULL) {
+                printf("Window could not be created! SDL_Error: %s", SDL_GetError());
+                success = false;
+            } else {
+                this->windowSurface = SDL_GetWindowSurface(window);
+                this->framebuffer = SDL_CreateRGBSurface(0, this->frameWidth, this->frameHeight, 32, 0, 0, 0, 0);
+                this->pointBuffer = std::vector<std::vector<Attribs>>(this->frameWidth, std::vector<Attribs>(this->frameHeight, Attribs()));
+            }
+        }
         return success;
 }
--- a3-main/src/Basic_Ray_Tracer.hpp
+++ a3-main/src/Basic_Ray_Tracer.hpp
@@ -1,10 +1,23 @@
 #include <string>
+#include <SDL2/SDL.h>
+#include <glm/glm.hpp>
 
-// using namespace glm;
+using namespace glm;
 using namespace std;
 
 class Ray_Tracer{
-
+    public:
     bool initialize(const std::string &title, int width, int height, int spp);
+    bool
+
+    private:
+    SDL_Window *window;
+    bool quit;
+    SDL_Surface* framebuffer = NULL;
+    SDL_Surface *windowSurface = NULL; 
+    int frameWidth;
+    int frameHeight;
+    int displayScale;
+    int spp; // Samples-per-pixel
 
 };
