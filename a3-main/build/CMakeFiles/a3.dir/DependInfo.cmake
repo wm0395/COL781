dadfd339a4985208200bdadabef453a33af3831d
wm0395 <wmharshit@gmail.com>
2024-03-23 17:48:53 +0530

    lighting1

--- a3-main/build/CMakeFiles/a3.dir/DependInfo.cmake
+++ a3-main/build/CMakeFiles/a3.dir/DependInfo.cmake
@@ -8,10 +8,10 @@ set(CMAKE_DEPENDS_LANGUAGES
 
 # The set of dependency files which are needed:
 set(CMAKE_DEPENDS_DEPENDENCY_FILES
-  "/home/kanav/COL781/COL781_Assignments/a3-main/deps/src/gl.c" "CMakeFiles/a3.dir/deps/src/gl.c.o" "gcc" "CMakeFiles/a3.dir/deps/src/gl.c.o.d"
-  "/home/kanav/COL781/COL781_Assignments/a3-main/src/Camera.cpp" "CMakeFiles/a3.dir/src/Camera.cpp.o" "gcc" "CMakeFiles/a3.dir/src/Camera.cpp.o.d"
-  "/home/kanav/COL781/COL781_Assignments/a3-main/src/Scene.cpp" "CMakeFiles/a3.dir/src/Scene.cpp.o" "gcc" "CMakeFiles/a3.dir/src/Scene.cpp.o.d"
-  "/home/kanav/COL781/COL781_Assignments/a3-main/src/Shape.cpp" "CMakeFiles/a3.dir/src/Shape.cpp.o" "gcc" "CMakeFiles/a3.dir/src/Shape.cpp.o.d"
+  "/home/wm0395/COL781_Assignments/a3-main/deps/src/gl.c" "CMakeFiles/a3.dir/deps/src/gl.c.o" "gcc" "CMakeFiles/a3.dir/deps/src/gl.c.o.d"
+  "/home/wm0395/COL781_Assignments/a3-main/src/Camera.cpp" "CMakeFiles/a3.dir/src/Camera.cpp.o" "gcc" "CMakeFiles/a3.dir/src/Camera.cpp.o.d"
+  "/home/wm0395/COL781_Assignments/a3-main/src/Scene.cpp" "CMakeFiles/a3.dir/src/Scene.cpp.o" "gcc" "CMakeFiles/a3.dir/src/Scene.cpp.o.d"
+  "/home/wm0395/COL781_Assignments/a3-main/src/Shape.cpp" "CMakeFiles/a3.dir/src/Shape.cpp.o" "gcc" "CMakeFiles/a3.dir/src/Shape.cpp.o.d"
   )
 
 # Targets to which this target links.
--- a3-main/build/CMakeFiles/example1.1.dir/flags.make
+++ a3-main/build/CMakeFiles/example1.1.dir/flags.make
@@ -4,7 +4,7 @@
 # compile CXX with /usr/bin/c++
 CXX_DEFINES = 
 
-CXX_INCLUDES = -I/home/kanav/COL781/COL781_Assignments/a3-main/deps/include -isystem /usr/lib -isystem /usr/include/SDL2
+CXX_INCLUDES = -I/home/wm0395/COL781_Assignments/a3-main/deps/include -isystem /usr/lib -isystem /usr/include/SDL2
 
 CXX_FLAGS = -std=gnu++11
 
